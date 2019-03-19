3fd7903841d0aa975e4acc6daeea4dd8b215acbd
“Didar” <enderoasis@gmail.com>
2019-03-20 02:12:43 +0600

    da

--- ref/db.php
+++ ref/db.php
@@ -1,5 +1,5 @@
 <?php
-require '/libs/rb-mysql.php';
+require 'rb-mysql.php';
 R::setup( 'mysql:host=srv-pleskdb33.ps.kz:3306;dbname=saicopro_2','saico_admin', 'qtH^462y' );
 
 if ( !R::testconnection() )
