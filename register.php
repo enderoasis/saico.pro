dadc7985fd71933d7e53768ebd2b79548b812245
“Didar” <enderoasis@gmail.com>
2019-03-22 00:55:06 +0600

    Update register.php

--- register.php
+++ register.php
@@ -44,22 +44,9 @@ require 'db.php';
 
 		if ( empty($errors) )
 		{
-			$rf = basename(parse_url('https://education.saico.pro/register.php/1',  PHP_URL_PATH));
-			$email = $data['email'];
-			$pass = $data['password'];
-
-
-
-										$url = "https://partners.saico.pro/nrfpp?name=&email=".$email."&password=".$pass."&referer=".$ref;
-											$curl = curl_init();
-											curl_setopt($curl, CURLOPT_URL, $url);
-											curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-											curl_setopt($curl, CURLOPT_HEADER, false);
-											$data = curl_exec($curl);
-											curl_close($curl);
 
 			//ошибок нет, теперь регистрируем
-
+			$rf = basename(parse_url('https://education.saico.pro/register.php/1',  PHP_URL_PATH));
 			$user = R::dispense('users');
 			$user->login = $data['login'];
 			$user->email = $data['email'];
@@ -67,19 +54,13 @@ require 'db.php';
 			$user->password = password_hash($data['password'], PASSWORD_DEFAULT); //пароль нельзя хранить в открытом виде, мы его шифруем при помощи функции password_hash для php > 5.6
 			R::store($user);
 
-header("Content-Type: text/html; charset=UTF-8");
-header('Refresh: 3; url=login.php');
+
                 //Составляем зашифрованный и уникальный token
 
 
 
-<<<<<<< HEAD
 								header("Content-Type: text/html; charset=UTF-8");
 								header('Refresh: 3; url=/login.php');
-=======
-
-
->>>>>>> parent of 6ab5a1b... Update register.php
                 //Добавляем данные в таблицу confirm_users
                 //$confirmuser = R::dispense('confirmusers');
 			//$confirmuser->email = $data['email'];
@@ -89,26 +70,26 @@ header('Refresh: 3; url=login.php');
 
 
                     //Составляем заголовок письма
-                    $subject = "Подтверждение почты на сайте ".$_SERVER['HTTP_HOST'];
+                  //  $subject = "Подтверждение почты на сайте ".$_SERVER['HTTP_HOST'];
 
                     //Устанавливаем кодировку заголовка письма и кодируем его
-                    $subject = "=?utf-8?B?".base64_encode($subject)."?=";
+                    //$subject = "=?utf-8?B?".base64_encode($subject)."?=";
 
                     //Составляем тело сообщения
-                    $message = 'Здравствуйте! <br/> <br/> Сегодня '.date("d.m.Y", time()).', неким пользователем была произведена регистрация на сайте <a href="'.$address_site.'">'.$_SERVER['HTTP_HOST'].'</a> используя Ваш email. Если это были Вы, то, пожалуйста, подтвердите адрес вашей электронной почты, перейдя по этой ссылке: <a href="'.$address_site.'activation.php?token='.$token.'&email='.$email.'">'.$address_site.'activation/'.$token.'</a> <br/> <br/> В противном случае, если это были не Вы, то, просто игнорируйте это письмо. <br/> <br/> <strong>Внимание!</strong> Ссылка действительна 24 часа. После чего Ваш аккаунт будет удален из базы.';
+                    //$message = 'Здравствуйте! <br/> <br/> Сегодня '.date("d.m.Y", time()).', неким пользователем была произведена регистрация на сайте <a href="'.$address_site.'">'.$_SERVER['HTTP_HOST'].'</a> используя Ваш email. Если это были Вы, то, пожалуйста, подтвердите адрес вашей электронной почты, перейдя по этой ссылке: <a href="'.$address_site.'activation.php?token='.$token.'&email='.$email.'">'.$address_site.'activation/'.$token.'</a> <br/> <br/> В противном случае, если это были не Вы, то, просто игнорируйте это письмо. <br/> <br/> <strong>Внимание!</strong> Ссылка действительна 24 часа. После чего Ваш аккаунт будет удален из базы.';
 
                     //Составляем дополнительные заголовки для почтового сервиса mail.ru
                     //Переменная $email_admin, объявлена в файле dbconnect.php
-                    $headers = "FROM: $email_admin\r\nReply-to: $email_admin\r\nContent-type: text/html; charset=utf-8\r\n";
+                    //$headers = "FROM: $email_admin\r\nReply-to: $email_admin\r\nContent-type: text/html; charset=utf-8\r\n";
 
                     //Отправляем сообщение с ссылкой для подтверждения регистрации на указанную почту и проверяем отправлена ли она успешно или нет.
-                    if(mail($email, $subject, $message, $headers)){
-                        $_SESSION["success_messages"] = "<h4 class='success_message'><strong>Регистрация прошла успешно!!!</strong></h4><p class='success_message'> Теперь необходимо подтвердить введенный адрес электронной почты. Для этого, перейдите по ссылке указанную в сообщение, которую получили на почту ".$email." </p>";
+                    //if(mail($email, $subject, $message, $headers)){
+                      //  $_SESSION["success_messages"] = "<h4 class='success_message'><strong>Регистрация прошла успешно!!!</strong></h4><p class='success_message'> Теперь необходимо подтвердить введенный адрес электронной почты. Для этого, перейдите по ссылке указанную в сообщение, которую получили на почту ".$email." </p>";
 
                         //Отправляем пользователя на страницу регистрации и убираем форму регистрации
-                        header("HTTP/1.1 301 Moved Permanently");
-                        header("Location: ".$address_site."/login.php");
-                        exit();
+                        //header("HTTP/1.1 301 Moved Permanently");
+                        //header("Location: ".$address_site."/login.php");
+                        //exit();
 
                     }
 
