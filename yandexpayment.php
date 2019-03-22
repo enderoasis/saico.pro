9764e0a7825bc50252ff0b3834aff03bbb98951b
“Didar” <enderoasis@gmail.com>
2019-03-23 01:05:37 +0600

    Update yandexpayment.php

--- yandexpayment.php
+++ yandexpayment.php
@@ -20,13 +20,7 @@
 							<!-- Header -->
 								<header id="header">
 									<a href="index.html" class="logo"><strong>Обучение</strong> by Saico.pro</a>
-									<ul class="icons">
-										<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-										<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-										<li><a href="#" class="icon fa-snapchat-ghost"><span class="label">Snapchat</span></a></li>
-										<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-										<li><a href="#" class="icon fa-medium"><span class="label">Medium</span></a></li>
-									</ul>
+								
 								</header>
 
 							<!-- Content -->
@@ -49,7 +43,7 @@
 								<strong>2)Наш счёт,официально подтверждён платёжной системой.<br></strong>
 								<strong>3)Вы будете перенаправлены на платёжную систему Яндекса.<br></strong>
 								<strong>4) Мы получаем только те данные,которые вы указываете у нас на сайте.<br></strong>
-								<strong>5)Укажите действительную почту,это очень важно!Если вы уже зарегистрировались у нас на сайте укажите ту же почту при оплате!</strong>
+								<strong>5)Указанную ниже сумму в 2000р не менять,иначе оплата не произведётся.</strong>
 								</section>
 <iframe src="https://money.yandex.ru/quickpay/shop-widget?writer=seller&targets=%D0%9E%D0%BF%D0%BB%D0%B0%D1%82%D0%B0%20%D0%B7%D0%B0%20%D0%BA%D1%83%D1%80%D1%81%20%D0%BE%D1%82%20Saico%20Education&targets-hint=&default-sum=2000&button-text=11&payment-type-choice=on&mail=on&hint=&successURL=https%3A%2F%2Feducation.saico.pro%2F&quickpay=shop&account=410018884562159" width="100%" height="223" frameborder="0" allowtransparency="true" scrolling="no"></iframe>
 
