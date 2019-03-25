977bbc13740fc2f56afc248121fb127b8d0c382b
“Didar” <enderoasis@gmail.com>
2019-03-25 23:22:26 +0600

    Create main.php

---
+++ main.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Обучение by Saico.pro</title>
+		<meta charset="utf-8" />
+		    <link rel="icon" href="https://partners.saico.pro/favicon.png">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="/assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+
+							<!-- Header -->
+								<header id="header">
+									<a href="index.php" class="logo"><strong>Обучение</strong> by Saico.pro</a>
+									<ul class="icons">
+
+																			<li><a href="index.php" class="button">Вернуться на главную</a></li>
+								</header>
+
+							<!-- Content -->
+								<section>
+									<header class="main" >
+										<h1>К сожалению , вам отказано в доступе к материалам</h1>
+									</header>
+
+
+
+									<h1>Почему?</h1>
+									<p>Вы неосуществили оплату за курс.<br>
+									<a  href="/yandexpayment.php" class="button">После оплаты вам откроется доступ ко всем имеющимся материалам курса.</a><br>
+									Прежде всего,мы нацелены на результаты и успехи наших клиентов.
+
+									<br></p>
+
+								</section>
+
+
+						</div>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="/assets/js/jquery.min.js"></script>
+			<script src="/assets/js/browser.min.js"></script>
+			<script src="/assets/js/breakpoints.min.js"></script>
+			<script src="/assets/js/util.js"></script>
+			<script src="/assets/js/main.js"></script>
+
+	</body>
+</html>
