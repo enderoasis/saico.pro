01857a2083b2b7d4a394fc9656e4df138bf790ab
“Didar” <enderoasis@gmail.com>
2019-03-27 01:07:25 +0600

    Update check.php

--- check.php
+++ check.php
@@ -3,9 +3,10 @@ require 'db.php';
 session_start();
 
 	$check	=  $_SESSION['mail'];
- $st = 1;
+
  	$fon = R::findOne('users', 'email = ?', array($check));
   if ($fon) {
+    $st = 1;
   $fnd	= R::findOne('users','status = ?', array($st));
   }
 if ($fnd) {
@@ -17,6 +18,7 @@ else {
   $fon1 = R::findOne('payments', 'email = ?', array($check));
 }
   if ($fon1) {
+    $st = 1;
     $fnd1 = R::findOne('payments','status = ?', array($st));
   }
   if ($fnd1) {
