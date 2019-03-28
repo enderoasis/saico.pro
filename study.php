2bfad7379f7fe5f2b8a0eb31a13bc5ea7b2f732e
“Didar” <enderoasis@gmail.com>
2019-03-28 16:05:18 +0600

    Update study.php

--- study.php
+++ study.php
@@ -14,7 +14,7 @@ header( 'Refresh: 0; url=login.php' );
   if ($cets) {
       $st = 1;
     $state = R::find('users','status = ?', array( $st ));
-    if ($state) {
+    if ($state == 1) {
 
       $_SESSION['status'] = $st;
     }
@@ -22,7 +22,7 @@ header( 'Refresh: 0; url=login.php' );
   if (!$cets) {
     $st1 = 1;
     $state1 = R::find('payments','status = ?', array( $st1 ));
-    if ($state1) {
+    if ($state1 == 1) {
 
       $_SESSION['status'] = $st;
 
