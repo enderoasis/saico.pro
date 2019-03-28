97460cddd40e4bfdbcfb940017acc14e2c93a908
“Didar” <enderoasis@gmail.com>
2019-03-28 17:03:30 +0600

    Update check.php

--- check.php
+++ check.php
@@ -11,19 +11,19 @@ session_start();
   if ($cets) {
       $st = 1;
     $state = R::find('users','status = ?', array( $st ));
-    if ($state == 1) {
+    if ($state) {
 
-      $_SESSION['status'] = $st;
+      $_SESSION['status'] = $s1;
       header( 'Refresh: 0; url=study.php' );
     }
   }
   if (!$cets) {
     $st1 = 1;
     $state1 = R::find('payments','status = ?', array( $st1 ));
-    if ($state1 == 1) {
+    if ($state1) {
 
-      $_SESSION['status'] = $st;
-
+      $_SESSION['status'] = $s1;
+  header( 'Refresh: 0; url=study.php' );
     }
 
     else {
