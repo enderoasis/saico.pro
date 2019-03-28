177b468a7ee4c8d786151c6b9b67a343864c5057
“Didar” <enderoasis@gmail.com>
2019-03-28 17:37:14 +0600

    Update login.php

--- login.php
+++ login.php
@@ -11,10 +11,10 @@ session_start();
 
 		if ( $user )
 		{
-			//логин существует
+		$user2 = R::find('users','status = ?', array( 1 ));
 			if ( password_verify($data1['password'], $user->password))
 			{
-
+				$_SESSION['stat'] = $user2;
  			 $_SESSION['mail'] = $_POST['email'];
 			$_SESSION['logged_user'] = $user;
 
