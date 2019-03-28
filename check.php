d3b4608580d14f0e7df18d9143d6005235b004a8
“Didar” <enderoasis@gmail.com>
2019-03-28 17:33:57 +0600

    Update check.php

--- check.php
+++ check.php
@@ -3,11 +3,12 @@ require 'db.php';
 session_start();
 
 $_SESSION['mail'] = $pc;
-if ($user->email == $pc) {
+if ($user->status == 1) {
+
 	echo "HI";
 }
 else {
-	echo "BYE";
+  echo "BYE";
 }
 
 	?>
