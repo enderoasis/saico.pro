d7522b956c19383f1f747cce72b9c16d486a3cd1
“Didar” <enderoasis@gmail.com>
2019-03-28 19:11:13 +0600

    Update check.php

--- check.php
+++ check.php
@@ -2,11 +2,15 @@
 require 'db.php';
 session_start();
 
-$_SESSION['mail'] = $pc;
+$pc = $_SESSION['mail'];
 
 if ($user->email == $pc) {
-
-$getstatus = R::getAll('SELECT status * FROM users WHERE email = :mail', array(':mail' => $pc) );
+$stat = 1;
+//$getstatus = R::find('users', 'email = :pc' ,' status = :stat' , array('pc' =>$pc, 'stat'=>$stat ));
+$getstatus = R::exec('SELECT 'status' FROM `users` WHERE `email` = ?', array(
+    $pc
+));
+var_dump ($getstatus);
 }
  if( $getstatus == 1 ) {
 	 $_SESSION['cond'] = $getstatus;
