1f4a3dd481dc8c8ef583dd6285c3f6ad636aca95
“Didar” <enderoasis@gmail.com>
2019-03-28 17:29:50 +0600

    da

--- check.php
+++ check.php
@@ -1,37 +1,13 @@
 <?php
 require 'db.php';
 session_start();
-if (isset($_SESSION['status'])) {
-	  header( 'Refresh: 0; url=main.php' );
-}
 
+$_SESSION['mail'] = $pc;
+if ($user->email == $pc) {
+	echo "HI";
+}
 else {
-
-
-	$check	=  $_SESSION['mail'];
-
-  $cets = R::findOne('users', 'email = ?', array($check));
+	echo "BYE";
 }
-  if ($cets) {
-      $st = 1;
-    $state = R::find('users','status = ?', array( $st ));
-    if ($state) {
 
-      $_SESSION['status'] = "OK";
-      header( 'Refresh: 0; url=study.php' );
-    }
-  }
-  if (!$cets) {
-    $st1 = 1;
-    $state1 = R::find('payments','status = ?', array( $st1 ));
-    if ($state1) {
-
-      $_SESSION['status'] = "OK";
-  header( 'Refresh: 0; url=study.php' );
-    }
-
-    else {
-      header( 'Refresh: 0; url=main.php' );
-    }
-  }
 	?>
