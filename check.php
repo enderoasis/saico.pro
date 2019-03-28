ce623b76465e5477a7e302f8b95b9bbd8c52ef56
“Didar” <enderoasis@gmail.com>
2019-03-28 19:23:27 +0600

    Update check.php

--- check.php
+++ check.php
@@ -11,7 +11,7 @@ $getstatus = R::exec('SELECT `status` FROM `users` WHERE `email` = ?', array($pc
 
 var_dump ($getstatus);
 }
- if( $getstatus == 1 ) {
+ if( $getstatus == $stat ) {
 	 $_SESSION['cond'] = $getstatus;
 	  header( 'Refresh: 0; url=study.php' );
  }
