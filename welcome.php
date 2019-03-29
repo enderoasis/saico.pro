e2f839a7e0bd5650b448f61d50f803b0714199d6
“Didar” <enderoasis@gmail.com>
2019-03-29 22:10:44 +0600

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -4,7 +4,7 @@ require 'db.php';
 session_start();
 $pc2 = $_SESSION['pc2'];
 $identification = R::findOne('users', ' email = ? ', array($pc2));
-$limit = 201;
+$limit = 3;
 if ($identification->id <= $limit) {
 	$identification->status = '1';
 	R::store($identification);
