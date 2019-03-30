89911fe0c3c1310a2fc1ab1ee4226401ce99f4af
“Didar” <enderoasis@gmail.com>
2019-03-31 00:49:38 +0600

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 require 'libs/rb-mysql.php';
-R::setup( 'mysql:host=srv-pleskdb33.ps.kz:3306;dbname=saicopro_2','saico_admin', 'qtH^462y' ); 
+R::setup( 'mysql:host=srv-pleskdb33.ps.kz:3306;dbname=saicopro_2','saico_admin', 'qtH^462y' );
 
 if ( !R::testconnection() )
 {
 		exit ('Нет соединения с базой данных');
 }
 $address_site = "auth.saico.pro";
-$email_admin = "info@saico.pro";
+$email_admin = "admin@saico.pro";
 
 ?>
