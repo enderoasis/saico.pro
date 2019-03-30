57d19d60381a69e1c9da16aaaa949f3babfc0768
“Didar” <enderoasis@gmail.com>
2019-03-31 01:13:15 +0600

    вф

---
+++ panel.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Вывод данных</title>
+</head>
+
+<body>
+<fieldset>
+<form method="post" action="search.php">
+<label for="first_name">№ Транзакции:</label><br/>
+<input type="text" name="first_name" size="30"><br/>
+
+<input id="submit" type="submit" value="Найти"><br/>
+</form>
+</fieldset>
+</body>
+</html>
