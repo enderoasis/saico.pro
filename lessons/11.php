df5ba5d53cf16ef6be04ab986e06b00b574bf79a
“Didar” <enderoasis@gmail.com>
2019-04-01 00:56:38 +0600

    da

---
+++ lessons/11.php
@@ -0,0 +1,323 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+	<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=utf-8">
+	<TITLE></TITLE>
+	<META NAME="GENERATOR" CONTENT="LibreOffice 4.1.6.2 (Linux)">
+	<META NAME="AUTHOR" CONTENT="Пользователь Windows">
+	<META NAME="CREATED" CONTENT="20190217;91200000000000">
+	<META NAME="CHANGEDBY" CONTENT="Пользователь Windows">
+	<META NAME="CHANGED" CONTENT="20190217;91200000000000">
+	<META NAME="AppVersion" CONTENT="14.0000">
+	<META NAME="DocSecurity" CONTENT="0">
+    <link rel="stylesheet" href="assets/css/main.css" />
+        <link rel="icon" href="https://partners.saico.pro/favicon.png">
+	<META NAME="HyperlinksChanged" CONTENT="false">
+	<META NAME="LinksUpToDate" CONTENT="false">
+	<META NAME="ScaleCrop" CONTENT="false">
+	<META NAME="ShareDoc" CONTENT="false">
+	<STYLE TYPE="text/css">
+	<!--
+		@page { margin-left: 1.18in; margin-right: 0.59in; margin-top: 0.79in; margin-bottom: 0.79in }
+		P { margin-bottom: 0.08in; direction: ltr; widows: 2; orphans: 2 }
+		A:link { color: #0000ff; so-language: zxx }
+	-->
+	</STYLE>
+</HEAD>
+<BODY LANG="ru-RU" LINK="#0000ff" DIR="LTR">
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%"><A NAME="_GoBack"></A>
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Здравствуйте,
+друзья! Многие компании уже успели по
+достоинству оценить эффективность
+такого инструмента продвижения своих
+товаров и услуг, как Инстаграм. Кстати,
+совсем недавно </FONT></FONT><A HREF="http://www.pro-smm.com/populyarnye-socialnye-seti/"><FONT COLOR="#0000ff"><FONT FACE="Times New Roman, serif"><FONT SIZE=3><U>Инстаграм
+опередил Твиттер по количеству активных
+пользователей</U></FONT></FONT></FONT></A><FONT FACE="Times New Roman, serif"><FONT SIZE=3>,
+которых у него насчитывается уже более
+500 миллионов!</FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Итак, идем
+дальше. Ниже вы узнаете о</FONT></FONT><FONT FACE="Times New Roman, serif"><FONT SIZE=3><B>
+7 самых распространенных ошибках в
+продвижении компании или бренда в
+Инстаграм</B></FONT></FONT><FONT FACE="Times New Roman, serif"><FONT SIZE=3>.</FONT></FONT></P>
+<P STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=5><B>1. Закрытие
+аккаунтов Инстаграм от посторонних</B></FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<IMG SRC="7__html_3cf68b5e.jpg" NAME="Рисунок 4" ALIGN=BOTTOM WIDTH=165 HEIGHT=165 BORDER=0></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Владелец
+аккаунта в Инстаграм может запретить
+доступ к контенту, к примеру,
+незарегистрированным пользователям
+или пользователям, которые не являются
+его подписчиками. В результате, человеку,
+чтобы ознакомиться с ассортиментом
+ваших товаров, необходимо регистрироваться
+в соц. сети или подписываться на аккаунт
+(плюс еще ждать подтверждения на подписку
+с вашей стороны). А это дополнительные
+действия и сложности, которые просто
+несовместимы с эффективным продвижением.
+Чем больше препятствий вы будете
+создавать на пути потенциального
+клиента, тем меньше шансов, что он станет
+клиентом реальным. Скорее всего, человек
+просто подпишется на уже открытый
+аккаунт вашего конкурента.</FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Это базовая
+ошибка, но, к сожалению, время от времени,
+мы все еще наталкиваемся на такие вот
+закрытые аккаунты брендов. Если закрытые
+аккаунты для личных профилей — это
+нормально, то для компаний это, как
+минимум, странно.</FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3><I>Фишка для
+самых любопытных.</I></FONT></FONT><FONT FACE="Times New Roman, serif"><FONT SIZE=3>
+Если вам очень интересно посмотреть,
+что скрывается за скрытым аккаунтом в
+Инстаграм, то вы можете погуглить всякие
+программки типа «private profile viewer on Instagram»
+и Гугл вам выдаст массу программ, которые
+помогут открыть для просмотра нужные
+профили.</FONT></FONT></P>
+<P STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=5><B>2. Минимальное
+количество контактных данных или их
+полное отсутствие</B></FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Потенциальные
+клиенты должны знать, каким именно
+способом с вами можно связаться, чтобы
+задать вопрос или купить ваш продукт
+или услугу. Поэтому, в идеале, следует
+указывать несколько способов связи, к
+примеру, vk, email, телефон, адрес. Некоторые
+владельцы аккаунтов в Инстаграм указывают
+только один способ связи, к примеру,
+Skype или WhatsApp, не думая о том, что далеко
+не все клиенты пользуются данными
+программами.</FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Вот один
+реальный пример того, что делать
+нежелательно. Кстати, лайки здесь
+накрученные, так как аккаунт совсем
+дохлый. Не обижайтесь на меня админы
+аккаунта за эти слова, но на данный
+момент он просто неэффективен. В описании
+предлагается связь с компанией через
+WhatsApp или Viber. У меня, например, нет ни
+того, ни другого. Что мне делать? Звонить
+я не буду. Эл. почта или САЙТ! не указаны.
+Честно говоря, у меня, как потенциального
+клиента, этот аккаунт не вызывает доверия
+и я его просто закрою и пойду искать
+ободки в другом месте.</FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<IMG SRC="7__html_86ae54ec.jpg" NAME="Рисунок 3" ALIGN=BOTTOM WIDTH=473 HEIGHT=235 BORDER=0></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=5><B>3. Очень
+частые обновления</B></FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Нежелательно
+публиковать новые посты слишком часто
+— особенно один за другим! В противном
+случае вы можете быстро надоесть своим
+подписчикам (в результате чего они от
+вас отпишутся). Я лично отписываюсь от
+таких аккаунтов сразу. Если вы понаблюдаете
+за активностью успешных на Инстаграм
+брендов, то увидите, что подобным спамом
+они не занимаются. Как всегда, </FONT></FONT><FONT FACE="Times New Roman, serif"><FONT SIZE=3><B>качество,
+а не количество контента должно быть
+вашим ориентиром</B></FONT></FONT><FONT FACE="Times New Roman, serif"><FONT SIZE=3>.</FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Оптимальная
+частота обновления — 1-4 публикаций в
+сутки. Иногда количество обновлений
+может быть выше, но только в исключительных
+случаях, например, во время какой-то
+маркетинговой кампании, конкурса или
+мероприятия.</FONT></FONT></P>
+<P STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=5><B>4. Искусственная
+накрутка подписчиков в Инстаграм</B></FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-bottom: 0in; line-height: 100%"><FONT FACE="Times New Roman, serif"><FONT SIZE=3><BR>Принцип
+накрутки подписчиков в Инстаграм немного
+напоминает принцип накрутки подписчиков
+в том же Вконтакте. Другими словами,
+большая часть таких пользователей
+наверняка окажутся ботами, которым, по
+понятным причинам, не интересны ваши
+товары и услуги. И пусть вам сколько
+угодно твердят, что подписчики будут
+живые — не верьте таким сммщикам или
+компаниям! Массово живых, целевых
+подписчиков вам никто никогда не приведет
+(особенно за недорогую плату)! Это просто
+</FONT></FONT><FONT FACE="Times New Roman, serif"><FONT SIZE=3><U>не
+реально</U></FONT></FONT><FONT FACE="Times New Roman, serif"><FONT SIZE=3>!</FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Вы выбросите
+деньги на ветер. А ведь эти деньги можно
+было бы потратить на создание качественного
+контента и рекламные акции, которые как
+раз-то и привели бы к вам целевую
+аудиторию. Да, это тяжелее и дольше, но
+зато у вас будут реальные результаты,
+а не просто пустые, ничего не значащие
+цифры в профиле. К тому же, Инстаграм,
+время от времени, чистит базу пользователей
+от спаммеров и фейков и вы в одночасье
+можете потерять всех подписчиков,
+которых накрутили, а это, друзья, может
+очень сильно навредить репутации вашей
+компании! Плюс, это, конечно же деньги,
+потраченные в никуда.</FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Еще один
+аргумент против накрутки подписчиков
+— количество лайков и комментариев
+после искусственного их увеличения
+останется тем же самым, а это ой как
+бросается в глаза посетителям вашего
+аккаунта. Тут даже не нужно быть экспертом
+смм, чтобы понять, что происходит. Уверяю
+вас, цифры доверия вам не прибавляют.
+Если вы качественно выполняете вашу
+работу и люди вас ценят, то они будут
+подписываться на вас, даже если у вас
+пока что 10 фолловеров. Накручивая
+циферки, вы пускаете пыль в глаза прежде
+всего самому себе, а не кому-то другому.</FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Вот один
+реальный пример — компания по продаже
+украшений в интернете. У аккаунта более
+11 000 фолловеров и, в среднем, по 40-50 лайков
+на фото и, конечно же, почти никаких
+комментариев от покупателей или хотя
+бы заинтересовавшихся фолловеров…&nbsp;
+Ну какой вот толк от такой накрутки?
+Кого обманули?
+<IMG SRC="7__html_3ed0966f.jpg" NAME="Рисунок 2" ALIGN=BOTTOM WIDTH=479 HEIGHT=203 BORDER=0></FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Поэтому,
+друзья, не покупайте и не накручивайте
+ваши аккаунты, а лучше потратьте ваши
+средства на </FONT></FONT><A HREF="http://www.pro-smm.com/facebook-marketing-plan/"><FONT COLOR="#0000ff"><FONT FACE="Times New Roman, serif"><FONT SIZE=3><U>разработку
+плана продвижения</U></FONT></FONT></FONT></A><FONT FACE="Times New Roman, serif"><FONT SIZE=3>
+и </FONT></FONT><A HREF="http://www.pro-smm.com/get-more-likes-comments-reshares-on-facebook/"><FONT COLOR="#0000ff"><FONT FACE="Times New Roman, serif"><FONT SIZE=3><U>создание
+качественного контента</U></FONT></FONT></FONT></A><FONT FACE="Times New Roman, serif"><FONT SIZE=3>,
+которые и принесут вам реальные
+результаты, а не просто красивые цифры
+в профиле!</FONT></FONT></P>
+<P STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=5><B>5. Вопросы
+пользователей, оставленные без внимания</B></FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Довольно
+часто пользователи комментируют
+фотографии, например, высказывают
+положительное или отрицательное мнение
+касательно какого-либо товара, задают
+вопросы, связанные с характеристиками
+продукта, способами оплаты, доставки и
+т. д. Владельцу или менеджеру аккаунта
+нужно обязательно отвечать на все эти
+вопросы, поддерживать обратную связь
+с клиентами. Ведь если вы не будете
+читать их комментарии и отвечать на
+вопросы, то люди могут подумать, что за
+аккаунтом никто не следит и вам до них
+просто нет дела. В результате желание
+покупать товар отпадет само собой и
+пользователей от вас отпишется или,
+того хуже, уйдет к вашему более
+внимательному конкуренту.</FONT></FONT></P>
+<P STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=5><B>6. Массовая
+простановка лайков и массовая подписка
+на аккаунты пользователей</B></FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Все еще
+поддерживаю все ошибки, описанные в
+этой статье, включая накрутки, но больше
+не отношусь негативно к автоматизации.
+Если это делать с умом, то можно получать
+вполне неплохие результаты. </FONT></FONT>
+</P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Как правило,
+это делают для того, чтобы засветиться
+перед максимально большим количеством
+людей, привлечь к себе внимание
+потенциальных клиентов. Реальной пользы
+от подобных действий не так-то и много,
+ведь многим привлеченным пользователям
+ваши товары и услуги будут не интересны.
+К тому же, массфоловинг и&nbsp;масслайкинг
+не придают серьезности вашему бренду,
+так как выглядите вы при этом жалко.
+Есть огромное количество эффективных
+маркетинговых шагов, которые вы можете
+предпринять, чтобы привлечь пользователей
+к вашему аккаунту — конкурсы, </FONT></FONT><A HREF="http://www.pro-smm.com/social-media-buttons/"><FONT COLOR="#0000ff"><FONT FACE="Times New Roman, serif"><FONT SIZE=3><U>SMO</U></FONT></FONT></FONT></A><FONT FACE="Times New Roman, serif"><FONT SIZE=3>,
+оповещение реальных клиентов и т. д.</FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<IMG SRC="7__html_7e59224f.jpg" NAME="Рисунок 1" ALIGN=BOTTOM WIDTH=398 HEIGHT=204 BORDER=0></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Использовать
+массфоловинг и&nbsp;масслайкинг целесообразно
+лишь в том случае, когда ваш бизнес
+относится к какой-то узкой тематике и
+вам любыми способами нужно набрать
+целевую аудиторию. Эти методы отнимают
+огромное количество времени при очень
+скромных результатах (макс. 30% ответят
+вам взаимностью). Лучше потратить это
+время на что-то более стоящее, что
+принесет вам результаты в среднесрочной
+и долгосрочной перспективе, чем
+размениваться по мелочам, лайкая все
+подряд.</FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=5><B>7. Публикация
+постов лишь в вечернее время</B></FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Подобное
+мнение бытует среди людей, которые
+занимаются продвижением в соц. сети
+Вконтакте. Во многих пабликах ВКонтакте
+новые посты публикуются ближе к вечеру,
+так как именно в это время пользователи
+возвращаются домой со своей работы,
+учебы и т. д. Именно вечером большинству
+пользователей удобно просматривать
+свои странички в социальной сети, листать
+новости. Что касается Инстаграм, то
+здесь дела обстоят совершено по-другому.
+Судите сами — Инстаграм является
+мобильным приложением (для просмотра
+контента люди используют, главным
+образом смартфоны или планшетники). По
+этой причине, пользовательская активность
+по времени суток распределяется
+более&nbsp;или&nbsp;менее равномерно. К тому
+же, многие пользователи любят просматривать
+ленту сразу после пробуждения, во время
+уроков/работы и перед сном, поэтому
+публикуя только в одно и то же время, вы
+многое теряете!</FONT></FONT></P>
+<P ALIGN=JUSTIFY STYLE="margin-top: 0.19in; margin-bottom: 0.19in; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Успешного
+Вам продвижения!</FONT></FONT></P>
+<P STYLE="margin-bottom: 0.14in"><BR><BR>
+</P>
+</BODY>
+</HTML>
