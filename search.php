7a1fa1cc45d7b9026aeab144f611e7a16033df3e
“Didar” <enderoasis@gmail.com>
2019-03-31 23:36:27 +0600

    Update search.php

--- search.php
+++ search.php
@@ -6,8 +6,9 @@ $op = $_SESSION['opid'];
 $operation = R::findOne('payments', ' transid = ? ', array($op));
   if ($operation) {
 $operation->email = $mail;
-$operation->sum = $total;
-echo "<p>Почта пользователя:.$mail.Сумма оплаты:".$total";
+$operation->status = $state;
+printf('Текущий E-mail: %s', $mail);
+printf('Статус оплаты: %s', $state);
 }
 
 
