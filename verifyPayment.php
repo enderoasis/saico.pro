cf3ff41bb9a8fb535f7f64132f7c373c26829950
“Didar” <enderoasis@gmail.com>
2019-04-25 23:55:55 +0600

    Update verifyPayment.php

--- verifyPayment.php
+++ verifyPayment.php
@@ -29,6 +29,9 @@ if ($clients) {
      #rec {
        width: 300px;
        }
+       #rec2 {
+         width: 300px;
+         }
      </style>
    </head>
    <body>
@@ -43,7 +46,7 @@ if ($clients) {
        <form name="form1" method="post">
        <p><i>Укажите почту</i><input type="text" сlass="form-control" name="email" id="rec" value="<?php echo @$_POST['tra'];?>" size="40" /></p>
        <p>
-           <input type="submit" name="do_fix" class="button" id="rec" value="Восстановление" size="40">
+           <input type="submit" name="do_fix" class="button" id="rec2" value="Восстановление" size="40">
        </p>
      </form>
 
