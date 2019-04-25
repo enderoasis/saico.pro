71c3dc9c4cc39f59a58c4d79318a1930aa4ea0f1
“Didar” <enderoasis@gmail.com>
2019-04-26 00:48:28 +0600

    da

--- search.php
+++ search.php
@@ -1,23 +1,25 @@
 <?php
 session_start();
-require 'db.php';
-$global = $_POST;
-$mail = $global['tra'];
-$_SESSION = $mail;
-$mail2 = $_SESSION['reg'];
 
-$getmail = R::findOne('payments', ' email = ? ', array($mail));
+require 'db.php';
+// From inputs in verifyPayment
+$_SESSION['inp'] = $input;
+// From registration
+$m1 = $_SESSION['reg'];
+// From login.php
+$_SESSION['mail'] = $m2
+$getmail = R::findOne('payments', ' email = ? ', array($input));
 $stat1 = '1';
 if ($getmail->status == $stat1)
 {
-  $clients = R::findOne('users', ' email = ? ', array($_SESSION['mail']));
+  $clients = R::findOne('users', ' email = ? ', array($m2));}
   if ($clients) {
     $clients->status = '1';
     R::store($clients);
     header( 'Refresh: 0; url=check.php' );  // code...
   }
 
-}
+
 
 
 
--- verifyPayment.php
+++ verifyPayment.php
@@ -3,23 +3,7 @@ session_start();
 require 'db.php';
 $global = $_POST;
 $mail = $global['tra'];
-$_SESSION = $mail;
-$mail2 = $_SESSION['reg'];
-$fromlog =  $_SESSION['mail'];
-$getmail = R::findOne('payments', ' email = ? ', array($mail));
-$stat1 = '1';
-if ($getmail->status == $stat1)
-{
-  $clients = R::findOne('users', ' email = ? ', array($fromlog));
-  if ($clients) {
-    $clients->status = '1';
-    R::store($clients);
-    header( 'Refresh: 0; url=check.php' );  // code...
-  }
-
-}
-
-
+$_SESSION['inp']= $global['tra'];
 
  ?>
  <!DOCTYPE html>
