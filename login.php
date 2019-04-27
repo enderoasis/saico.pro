24ab49202d37d01f248fce035343893d058e9d1f
“Didar” <enderoasis@gmail.com>
2019-04-28 01:22:18 +0600

    da

--- login.php
+++ login.php
@@ -22,6 +22,7 @@ session_start();
 
  			 $_SESSION['mail'] = $_POST['email'];
 			$_SESSION['logged_user'] = $user;
+			$_SESSION['reg_user'] = $_POST['email'];
 
 			echo '<div style="color:green;">Вы авторизованы!<br/>.</div><hr>';
 		header( 'Refresh: 0; url=check.php' );
